e2ebdd64d271ea23978a3a87728047ab6bef6315
normandesjuniorzup <normandes.junior@zup.com.br>
2020-05-06 17:42:35 -0300

    Fix Kafka Recreation

--- terraform/kafka.tf
+++ terraform/kafka.tf
@@ -1,3 +1,7 @@
+resource "aws_kms_key" "kms" {
+  description = "example"
+}
+
 resource "aws_msk_cluster" "msk" {
   cluster_name           = "hibicode"
   kafka_version          = "2.4.1"
@@ -9,4 +13,30 @@ resource "aws_msk_cluster" "msk" {
     client_subnets = module.vpc.public_subnets
     security_groups = [ aws_security_group.allow_all.id ]
   }
+
+  encryption_info {
+    encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
+    
+    encryption_in_transit {
+      client_broker = "TLS_PLAINTEXT"
+      in_cluster    = true
+    }
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = false
+      }
+
+      node_exporter {
+        enabled_in_broker = false
+      }
+    }
+  }
+}
+
+output "bootstrap_brokers" {
+  description = "Plaintext connection host:port pairs"
+  value       = aws_msk_cluster.msk.bootstrap_brokers
 }
